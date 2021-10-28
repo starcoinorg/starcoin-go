8ed7115d5d321f0b3e82e3f88ff5edd28a3dcead
reiget <reigetget@gmail.com>
2021-10-28 14:29:22 +0800

    add context for interface

--- client/jsonrpc/subscribe.go
+++ client/jsonrpc/subscribe.go
@@ -1,6 +1,7 @@
 package jsonrpc
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/starcoinorg/starcoin-go/client/jsonrpc/transport"
@@ -13,7 +14,7 @@ func (c *Client) SubscriptionEnabled() bool {
 }
 
 // Subscribe starts a new subscription
-func (c *Client) Subscribe(callback func(b []byte), args interface{}) (func() error, error) {
+func (c *Client) Subscribe(context context.Context,callback func(b []byte), args interface{}) (func() error, error) {
 	pub, ok := c.transport.(transport.PubSubTransport)
 	if !ok {
 		return nil, fmt.Errorf("Transport does not support the subscribe method")
