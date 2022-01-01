5c46b68a38475d38955b7849c97cbd7cf0f444ac
yangjiefeng <yangjiefeng@gmail.com>
2022-01-01 19:22:18 +0800

    add unit test

--- client/starcoin_client_test.go
+++ client/starcoin_client_test.go
@@ -6,10 +6,13 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/holiman/uint256"
 	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/serde"
+
+	"github.com/starcoinorg/starcoin-go/core/consensus"
 	"github.com/starcoinorg/starcoin-go/types"
 
 	"github.com/blocktree/go-owcrypt"
@@ -471,4 +474,142 @@ func TestGetBlockInfoByNumber(t *testing.T) {
 	}
 	j, _ := json.Marshal(h)
 	fmt.Println(string(j))
+}
+
+func TestVerifyHeaderDifficulty(t *testing.T) {
+	client := NewStarcoinClient("https://halley-seed.starcoin.org")
+	//h, err := client.HeaderWithDifficultyInfoByNumber(context.Background(), 535439) // PASS!
+	h, err := client.HeaderWithDifficultyInfoByNumber(context.Background(), 535440)
+	if err != nil {
+		t.FailNow()
+	}
+	hdr, err := h.BlockHeader.ToTypesHeader()
+	if err != nil {
+		fmt.Printf("ToTypesHeader error: %s, height: %v, hash: %s", err.Error(), h.BlockHeader.Height, hex.EncodeToString([]byte(h.BlockHeader.BlockHash)))
+		t.FailNow()
+	}
+	expected, err := difficultyCalculator(client, hdr, h.BlockTimeTarget, uint64(h.BlockDifficutyWindow))
+
+	e := new(uint256.Int)
+	overflow := e.SetFromBig(expected)
+	if overflow {
+		fmt.Printf("verifyHeaderDifficulty, SetFromBig overflow: %d", expected)
+		t.FailNow()
+	}
+	hd := new(uint256.Int).SetBytes(hdr.Difficulty[:])
+	hb, err := hdr.ToHeaderBlob()
+	if err != nil {
+		fmt.Printf("verifyHeaderDifficulty, header.ToHeaderBlob error: %v", hdr)
+	}
+
+	ok, err := consensus.VerifyHeaderDifficulty(*e, *hd, hb, hdr.Nonce, hdr.Extra[:])
+	if !ok || err != nil {
+		fmt.Println(ok, err)
+		t.FailNow()
+	}
+}
+
+func difficultyCalculator(client StarcoinClient, blockHeader *types.BlockHeader, timeTarget uint64, difficultyWindow uint64) (*big.Int, error) {
+	var currentHeight uint64 = blockHeader.Number
+	//get last difficulty
+	var lastDifficulties []BlockDiffInfo
+	hash := blockHeader.ParentHash
+	for height := currentHeight - 1; height > currentHeight-difficultyWindow-1; height-- {
+		chdr, err := client.HeaderByNumber(context.Background(), height)
+		//hdr, err := GetHeaderByHash(native, hash, chainId)
+		if err != nil {
+			return nil, fmt.Errorf("difficultyCalculator, get header error: %s, height: %d, hash: %s", err.Error(), height, hex.EncodeToString(hash))
+		}
+		hdr, err := chdr.ToTypesHeader()
+		if err != nil {
+			return nil, fmt.Errorf("ToTypesHeader error: %s, height: %d, hash: %s", err.Error(), height, hex.EncodeToString(hash))
+		}
+		target := targetToDiff(new(uint256.Int).SetBytes(hdr.Difficulty[:]))
+		lastDifficulties = append(lastDifficulties, BlockDiffInfo{hdr.Timestamp, *target})
+		hash = hdr.ParentHash
+	}
+	nextTarget, err := getNextTarget(lastDifficulties, timeTarget)
+	return targetToDiff(&nextTarget).ToBig(), err
+}
+
+type BlockDiffInfo struct {
+	Timestamp uint64
+	Target    uint256.Int
+}
+
+func targetToDiff(target *uint256.Int) *uint256.Int {
+	bigint := &big.Int{}
+	diff, _ := uint256.FromBig(bigint.Div(MAXU256, target.ToBig()))
+	return diff
+}
+
+func getNextTarget(blocks []BlockDiffInfo, timePlan uint64) (uint256.Int, error) {
+	//todo online to debug level
+	//log.Infof("get next target: %v, time plan: %d", blocks, timePlan)
+	nextTarget := uint256.NewInt(0)
+	length := len(blocks)
+	if length < 1 {
+		return *nextTarget, fmt.Errorf("get next target blocks is null.")
+	}
+	if length == 1 {
+		return blocks[0].Target, nil
+	}
+
+	totalTarget := new(big.Int)
+	for _, block := range blocks {
+		totalTarget.Add(totalTarget, block.Target.ToBig())
+	}
+
+	totalTargetU256, overflow := uint256.FromBig(totalTarget)
+	if overflow {
+		return *nextTarget, fmt.Errorf("get next target, total target overflow: %d.", totalTarget)
+	}
+
+	lengthU256 := new(uint256.Int).SetUint64(uint64(length))
+	avgTarget := new(uint256.Int).Div(totalTargetU256, lengthU256)
+
+	var avgTime uint64
+	if length == 2 {
+		avgTime = blocks[0].Timestamp - blocks[1].Timestamp
+	}
+	if length > 2 {
+		latestTimestamp := blocks[0].Timestamp
+		totalBlockTime := uint64(0)
+		vblocks := 0
+		for idx, block := range blocks {
+			if idx == 0 {
+				continue
+			}
+			totalBlockTime = totalBlockTime + (latestTimestamp - block.Timestamp)
+			vblocks = vblocks + idx
+		}
+		avgTime = totalBlockTime / uint64(vblocks)
+	}
+	if avgTime == 0 {
+		avgTime = 1
+	}
+
+	timePlanU256 := new(uint256.Int).SetUint64(timePlan)
+	avgTimeU256 := new(uint256.Int).SetUint64(avgTime)
+	nextTarget.Div(avgTarget, timePlanU256)
+	_, overflow = nextTarget.MulOverflow(nextTarget, avgTimeU256)
+	if overflow {
+		return *nextTarget, fmt.Errorf("get next target, next target overflow: avgTimeU256: %d, nextTarget: %d, avgTimeU256: %d .", avgTimeU256, nextTarget, avgTimeU256)
+	}
+	tempNextTarget := nextTarget.Clone()
+	tempNumber := new(uint256.Int).SetUint64(2)
+	tempNextTarget.Div(tempNextTarget, tempNumber)
+	tempAvgTarget := new(uint256.Int).Div(avgTarget, tempNumber)
+	if tempNextTarget.Gt(avgTarget) {
+		nextTarget.Mul(avgTarget, tempNumber)
+	} else if nextTarget.Lt(tempAvgTarget) {
+		nextTarget = tempAvgTarget.Clone()
+	}
+	return *nextTarget, nil
+}
+
+var MAXU256 = &big.Int{}
+
+func init() {
+	MAXU256.SetString("115792089237316195423570985008687907853269984665640564039457584007913129639935", 10)
 }
